3294374dbed03a49c7bc224a487f41c4ec96c670
Phong Vo <vhphong@gmail.com>
2024-04-30 16:12:43 -0500

    Add existing project files to Git

---
+++ src/main/java/com/pnk/cbresiliencedemo/CbResilienceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pnk.cbresiliencedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbResilienceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbResilienceDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pnk/cbresiliencedemo/CbResilienceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnk.cbresiliencedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbResilienceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
